f7f17a9cb9821b0329f21234191b6bdbd220ac5b
BARATH A S <baracs023@rmkcet.ac.in>
2025-09-22 11:33:03 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,11 +1,57 @@
-package com.example.springapp.repository;
+package com.example.springapp.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
-import com.example.springapp.model.Employee;
+@Entity
+public class Employee {
+    @Id
+    private int employeeId;
+    private String employeeName;
+    private String employeeEmail;
+    private double salary;
+    private String department;
 
-@Repository
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+    public Employee(){
+
+    }
+   
+    public Employee(int employeeId, String employeeName, String employeeEmail, double salary, String department) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.employeeEmail = employeeEmail;
+        this.salary = salary;
+        this.department = department;
+    }
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+    public String getEmployeeName() {
+        return employeeName;
+    }
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
+    public void setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
    
 }
